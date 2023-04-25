38c7650ff9b5615b67ee2050a875b40b9a728f9d
Jean Vidal <j.vidalnunes@gmail.com>
2023-04-25 17:02:20 -0300

    made adjustments

--- hooks/blog/index.js
+++ hooks/blog/index.js
@@ -1,10 +1,6 @@
 
 export async function blogFetcher(url) {
     const res = await fetch(url, {
-        // headers: {
-        //     Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-        //     Accept: 'application/vnd.github+json'
-        // },
         method: "GET",
     });
 
